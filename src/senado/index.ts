5448f3015674f167fde073bd8dfaa9a2e318ce82
Javier López <graulopezjavier@gmail.com>
2023-06-27 11:17:35 -0500

    feat: incluir otros cuatrenios

--- src/senado/index.ts
+++ src/senado/index.ts
@@ -5,219 +5,241 @@ import { ProyectoDetailData } from '../crawler/detail-processor'
 import * as R from 'remeda';
 
 export function formatNumeroLegislativo(numero: NumeroIdentificador) {
-  return `${numero.numero}/${numero.year}`
+    return `${numero.numero}/${numero.year}`
 }
 // This is the class to call for the lifecycle of the information
 export class SenadoService {
-  constructor(private senadoRepository: SenadoRepository = new SenadoRepository()) { }
+    constructor(private senadoRepository: SenadoRepository = new SenadoRepository()) { }
 
-  async refreshProyectos(cuatrenio: string, legislatura: string) {
-    // Asserts they exist
-    const exists = await this.senadoRepository.getLegislatura(cuatrenio, legislatura)
-    if (exists === null) {
-      throw new Error(`Legislatura ${cuatrenio} does not exist`)
-    }
-    const data = await getLegislaturaProyectsBasicData(cuatrenio, legislatura)
-    console.log(
-      `Parsed proyects ${data.length} data from cuatrenio ${cuatrenio} and legislatura ${legislatura}`
-    )
-    await this.senadoRepository.upsertProyectosFromBasicData(legislatura, data)
-  }
-
-  async refreshProyectoDetalles(legislatura: string) {
-    const proyectos = await this.senadoRepository.getProyectosLegislatura(legislatura)
-    const chunks = R.chunk(proyectos, 5)
-    for (const chunk of chunks) {
-      await Promise.all(chunk.map(async (proyecto) => {
-        const detalle = await getProyectoDetails(proyecto);
-        if (!detalle) {
-          console.error(`Skipping proyecto: ${proyecto.numero}, no detail datata was found`)
-          return null
+    async refreshCuatrenio(cuatrenio: string) {
+        const legislaturas = await this.senadoRepository.getLegislaturasCuatrenio(cuatrenio)
+        for (const legis of legislaturas) {
+            await this.refreshLegislatura(cuatrenio, legis.title)
         }
-        console.log(`Updating proyecto: ${proyecto.numero}, with id: ${proyecto.id}`)
-        await db.transaction().execute(async (tx) => {
-          await this.senadoRepository.upsertProyectoDetalles(tx, proyecto.id, detalle)
-        })
-      }))
     }
-  }
+
+    async refreshLegislatura(cuatrenio: string, legislatura: string) {
+        await this.refreshProyectos(cuatrenio, legislatura)
+        await this.refreshProyectoDetalles(legislatura)
+    }
+
+    async refreshProyectos(cuatrenio: string, legislatura: string) {
+        // Asserts they exist
+        const exists = await this.senadoRepository.getLegislatura(cuatrenio, legislatura)
+        if (exists === null) {
+            throw new Error(`Legislatura ${cuatrenio} does not exist`)
+        }
+        const data = await getLegislaturaProyectsBasicData(cuatrenio, legislatura)
+        console.log(
+            `Parsed proyects ${data.length} data from cuatrenio ${cuatrenio} and legislatura ${legislatura}`
+        )
+        await this.senadoRepository.upsertProyectosFromBasicData(legislatura, data)
+    }
+
+    async refreshProyectoDetalles(legislatura: string) {
+        const proyectos = await this.senadoRepository.getProyectosLegislatura(legislatura)
+        const chunks = R.chunk(proyectos, 5)
+        for (const chunk of chunks) {
+            await Promise.all(chunk.map(async (proyecto) => {
+                const detalle = await getProyectoDetails(proyecto);
+                if (!detalle) {
+                    console.error(`Skipping proyecto: ${proyecto.numero}, no detail datata was found`)
+                    return null
+                }
+                console.log(`Updating proyecto: ${proyecto.numero}, with id: ${proyecto.id}`)
+                await db.transaction().execute(async (tx) => {
+                    await this.senadoRepository.upsertProyectoDetalles(tx, proyecto.id, detalle)
+                })
+            }))
+        }
+    }
 }
 
 class SenadoRepository {
-  constructor(
-    private comisionRepository: ComisionRepository = new ComisionRepository(),
-    private autorRepository: AutorRepository = new AutorRepository()
-  ) { }
+    constructor(
+        private comisionRepository: ComisionRepository = new ComisionRepository(),
+        private autorRepository: AutorRepository = new AutorRepository()
+    ) { }
 
-  async upsertProyectosFromBasicData(legislatura: string, proyecto: ProyectoBasicData[]) {
-    await this.comisionRepository.upsertComisiones([
-      ...new Set(proyecto.map((p) => p.comision)).values()
-    ])
-    await db.transaction().execute(async (tx) => {
-      for (const p of proyecto) {
-        await this.upsertProyectFromBasicData(tx, legislatura, p)
-      }
-    })
-  }
-
-  private async upsertProyectFromBasicData(
-    tx: DBTransaction,
-    legislatura: string,
-    data: ProyectoBasicData
-  ) {
-    const numeroSenado = formatNumeroLegislativo(data.numeroSenado)
-    const numeroCamara = data.numeroCamara ? formatNumeroLegislativo(data.numeroCamara) : null
-    const update = {
-      comisionId: tx.selectFrom('comision').select('id').where('nombre', '=', data.comision),
-      estado: data.estado.estado,
-      estadoAnotacion: data.estado.anotacion,
-      numero: numeroSenado,
-      numeroCamara,
-      titulo: data.titulo,
-      fechaRadicado: data.fechaRadicado.toISOString(),
-      legislaturaId: tx.selectFrom('legislatura').select('id').where('title', '=', legislatura),
-      url: data.url
+    async upsertProyectosFromBasicData(legislatura: string, proyecto: ProyectoBasicData[]) {
+        await this.comisionRepository.upsertComisiones([
+            ...new Set(proyecto.map((p) => p.comision)).values()
+        ])
+        await db.transaction().execute(async (tx) => {
+            for (const p of proyecto) {
+                await this.upsertProyectFromBasicData(tx, legislatura, p)
+            }
+        })
     }
-    // TODO: Replace with ON CONFLICT DO REPLACE
-    // https://www.sqlite.org/lang_conflict.html
-    const { id: proyectoId } = await tx
-      .insertInto('proyectoSenado')
-      .values(update)
-      .returning('id')
-      .onConflict((oc) => oc.doUpdateSet(update))
-      .executeTakeFirstOrThrow()
 
-    await this.createRelacionados(tx, proyectoId, data.proyectosAcumulados)
-    await this.autorRepository.syncAutoresWithProyecto(tx, proyectoId, data.autores)
-    return proyectoId
-  }
+    private async upsertProyectFromBasicData(
+        tx: DBTransaction,
+        legislatura: string,
+        data: ProyectoBasicData
+    ) {
+        const numeroSenado = formatNumeroLegislativo(data.numeroSenado)
+        const numeroCamara = data.numeroCamara ? formatNumeroLegislativo(data.numeroCamara) : null
+        const update = {
+            comisionId: tx.selectFrom('comision').select('id').where('nombre', '=', data.comision),
+            estado: data.estado.estado,
+            estadoAnotacion: data.estado.anotacion,
+            numero: numeroSenado,
+            numeroCamara,
+            titulo: data.titulo,
+            fechaRadicado: data.fechaRadicado.toISOString(),
+            legislaturaId: tx.selectFrom('legislatura').select('id').where('title', '=', legislatura),
+            url: data.url
+        }
+        // TODO: Replace with ON CONFLICT DO REPLACE
+        // https://www.sqlite.org/lang_conflict.html
+        const { id: proyectoId } = await tx
+            .insertInto('proyectoSenado')
+            .values(update)
+            .returning('id')
+            .onConflict((oc) => oc.doUpdateSet(update))
+            .executeTakeFirstOrThrow()
 
-  async upsertProyectoDetalles(tx: DBTransaction, proyectoId: number, detalles: ProyectoDetailData) {
-    await tx.replaceInto('proyectoSenadoDetalles')
-      .values({
-        proyectoId,
-        origen: detalles.origen,
-        tipoLey: detalles.tipoLey,
-        fechaEnvioComision: detalles.fechaEnvioComision?.toISOString(),
-        fechaPresentacion: detalles.fechaPresentacion?.toISOString(),
-        fechaAprobacionPrimerDebate: detalles.fechaAprobacionPrimerDebate?.toISOString(),
-        fechaAprobacionSegundoDebate: detalles.fechaAprobacionSegundoDebate?.toISOString(),
-        fechaConciliacion: detalles.fechaConciliacion?.toISOString()
-      }).execute()
-
-    await tx.replaceInto('proyectoSenadoPublicaciones')
-      .values({
-        proyectoId,
-        exposicionMotivos: detalles.publicaciones.exposicionMotivos,
-        primeraPonencia: detalles.publicaciones.primeraPonencia,
-        segundaPonencia: detalles.publicaciones.segundaPonencia,
-        textoPlenaria: detalles.publicaciones.textoPlenaria,
-        conciliacion: detalles.publicaciones.conciliacion,
-        objeciones: detalles.publicaciones.objeciones,
-        concepto: detalles.publicaciones.concepto,
-        textoRehecho: detalles.publicaciones.textoRehecho,
-        sentenciaCorte: detalles.publicaciones.sentenciaCorte,
-      }).execute()
-
-
-    // await tx.deleteFrom('ponenteProyecto')
-    //   .where('proyectoId', '=', proyectoId)
-    //   .execute()
-    //
-    // if (detalles.ponentes.length) {
-    //   await tx.insertInto('ponente')
-    //     .values(detalles.ponentes.map((p) => ({ nombre: p.ponente })))
-    //     .onConflict((oc) => oc.doNothing())
-    //     .execute()
-    //
-    //   await tx.insertInto('ponenteProyecto')
-    //     .values(detalles.ponentes.map((p) => ({
-    //       proyectoId,
-    //       ponenteId: tx.selectFrom('ponente').select('id').where('nombre', '=', p.ponente),
-    //       debate: p.debate,
-    //     })))
-    //     .execute()
-    // }
-  }
-
-  async getProyectosLegislatura(legislatura: string) {
-    return db
-      .selectFrom('proyectoSenado')
-      .select(['proyectoSenado.id', 'proyectoSenado.url', 'proyectoSenado.numero'])
-      .leftJoin('legislatura', 'legislatura.id', 'proyectoSenado.legislaturaId')
-      .where('legislatura.title', '=', legislatura)
-      // TODO: Remove this limit
-      .execute()
-  }
-
-  private async createRelacionados(
-    db: DBTransaction,
-    proyectoId: number,
-    relacionados: NumeroIdentificador[]
-  ) {
-    if (relacionados.length === 0) {
-      return
+        await this.createRelacionados(tx, proyectoId, data.proyectosAcumulados)
+        await this.autorRepository.syncAutoresWithProyecto(tx, proyectoId, data.autores)
+        return proyectoId
     }
-    await db
-      .replaceInto('proyectosRelacionados')
-      .values(
-        relacionados.map((r) => ({
-          proyectoId,
-          relacionadoNumero: formatNumeroLegislativo(r)
-        }))
-      )
-      .execute()
-  }
 
-  async getLegislatura(cuatrenio: string, legislatura: string) {
-    return db
-      .selectFrom('legislatura')
-      .selectAll()
-      .leftJoin('cuatrenio', 'cuatrenio.id', 'legislatura.cuatrenioId')
-      .where('cuatrenio.title', '=', cuatrenio)
-      .where('legislatura.title', '=', legislatura)
-      .executeTakeFirst()
-  }
+    async upsertProyectoDetalles(tx: DBTransaction, proyectoId: number, detalles: ProyectoDetailData) {
+        await tx.replaceInto('proyectoSenadoDetalles')
+            .values({
+                proyectoId,
+                origen: detalles.origen,
+                tipoLey: detalles.tipoLey,
+                fechaEnvioComision: detalles.fechaEnvioComision?.toISOString(),
+                fechaPresentacion: detalles.fechaPresentacion?.toISOString(),
+                fechaAprobacionPrimerDebate: detalles.fechaAprobacionPrimerDebate?.toISOString(),
+                fechaAprobacionSegundoDebate: detalles.fechaAprobacionSegundoDebate?.toISOString(),
+                fechaConciliacion: detalles.fechaConciliacion?.toISOString()
+            }).execute()
+
+        await tx.replaceInto('proyectoSenadoPublicaciones')
+            .values({
+                proyectoId,
+                exposicionMotivos: detalles.publicaciones.exposicionMotivos,
+                primeraPonencia: detalles.publicaciones.primeraPonencia,
+                segundaPonencia: detalles.publicaciones.segundaPonencia,
+                textoPlenaria: detalles.publicaciones.textoPlenaria,
+                conciliacion: detalles.publicaciones.conciliacion,
+                objeciones: detalles.publicaciones.objeciones,
+                concepto: detalles.publicaciones.concepto,
+                textoRehecho: detalles.publicaciones.textoRehecho,
+                sentenciaCorte: detalles.publicaciones.sentenciaCorte,
+            }).execute()
+
+
+        // await tx.deleteFrom('ponenteProyecto')
+        //   .where('proyectoId', '=', proyectoId)
+        //   .execute()
+        //
+        // if (detalles.ponentes.length) {
+        //   await tx.insertInto('ponente')
+        //     .values(detalles.ponentes.map((p) => ({ nombre: p.ponente })))
+        //     .onConflict((oc) => oc.doNothing())
+        //     .execute()
+        //
+        //   await tx.insertInto('ponenteProyecto')
+        //     .values(detalles.ponentes.map((p) => ({
+        //       proyectoId,
+        //       ponenteId: tx.selectFrom('ponente').select('id').where('nombre', '=', p.ponente),
+        //       debate: p.debate,
+        //     })))
+        //     .execute()
+        // }
+    }
+
+    async getProyectosLegislatura(legislatura: string) {
+        return db
+            .selectFrom('proyectoSenado')
+            .select(['proyectoSenado.id', 'proyectoSenado.url', 'proyectoSenado.numero'])
+            .leftJoin('legislatura', 'legislatura.id', 'proyectoSenado.legislaturaId')
+            .where('legislatura.title', '=', legislatura)
+            // TODO: Remove this limit
+            .execute()
+    }
+
+    private async createRelacionados(
+        db: DBTransaction,
+        proyectoId: number,
+        relacionados: NumeroIdentificador[]
+    ) {
+        if (relacionados.length === 0) {
+            return
+        }
+        await db
+            .replaceInto('proyectosRelacionados')
+            .values(
+                relacionados.map((r) => ({
+                    proyectoId,
+                    relacionadoNumero: formatNumeroLegislativo(r)
+                }))
+            )
+            .execute()
+    }
+
+    private buildGetLegislaturas() {
+        return db
+            .selectFrom('legislatura as legislatura')
+            .select(['legislatura.id', 'legislatura.title', 'legislatura.cuatrenioId', 'legislatura.inicio', 'legislatura.fin'])
+            .leftJoin('cuatrenio', 'cuatrenio.id', 'legislatura.cuatrenioId')
+    }
+
+    async getLegislatura(cuatrenio: string, legislatura: string) {
+        return this.buildGetLegislaturas()
+            .where('cuatrenio.title', '=', cuatrenio)
+            .where('legislatura.title', '=', legislatura)
+            .executeTakeFirst()
+    }
+
+    async getLegislaturasCuatrenio(cuatrenio: string) {
+        return this.buildGetLegislaturas()
+            .where('cuatrenio.title', '=', cuatrenio)
+            .execute()
+    }
 }
 
 class ComisionRepository {
-  constructor() { }
+    constructor() { }
 
-  async upsertComisiones(comisiones: string[]) {
-    await db
-      .insertInto('comision')
-      .values(comisiones.map((c) => ({ nombre: c })))
-      .onConflict((co) => co.doNothing())
-      .execute()
-  }
+    async upsertComisiones(comisiones: string[]) {
+        await db
+            .insertInto('comision')
+            .values(comisiones.map((c) => ({ nombre: c })))
+            .onConflict((co) => co.doNothing())
+            .execute()
+    }
 }
 
 class AutorRepository {
-  constructor() { }
+    constructor() { }
 
-  async syncAutoresWithProyecto(db: DBTransaction, proyectoId: number, autores: string[]) {
-    if (autores.length > 0) {
-      await db
-        .insertInto('autor')
-        .values(autores.map((a) => ({ nombre: a })))
-        .onConflict((co) => co.doNothing())
-        .execute()
+    async syncAutoresWithProyecto(db: DBTransaction, proyectoId: number, autores: string[]) {
+        if (autores.length > 0) {
+            await db
+                .insertInto('autor')
+                .values(autores.map((a) => ({ nombre: a })))
+                .onConflict((co) => co.doNothing())
+                .execute()
 
-      const autoresId = await db
-        .selectFrom('autor')
-        .select('id')
-        .where('nombre', 'in', autores)
-        .execute()
+            const autoresId = await db
+                .selectFrom('autor')
+                .select('id')
+                .where('nombre', 'in', autores)
+                .execute()
 
-      await db.deleteFrom('autorProyectos').where('proyectoId', '=', proyectoId).execute()
+            await db.deleteFrom('autorProyectos').where('proyectoId', '=', proyectoId).execute()
 
-      await db
-        .insertInto('autorProyectos')
-        .columns(['proyectoId', 'autorId'])
-        .values(autoresId.map((a) => ({ proyectoId, autorId: a.id })))
-        .execute()
-    } else {
-      console.log(`No autores, ${proyectoId}`)
+            await db
+                .insertInto('autorProyectos')
+                .columns(['proyectoId', 'autorId'])
+                .values(autoresId.map((a) => ({ proyectoId, autorId: a.id })))
+                .execute()
+        } else {
+            console.log(`No autores, ${proyectoId}`)
+        }
     }
-  }
 }
--- src/utils/refresh.ts
+++ src/utils/refresh.ts
@@ -4,20 +4,34 @@ import { Command } from 'commander'
 const program = new Command()
 program.description('Refreshes the data from the senado')
 program.requiredOption('--cuatrenio <string>', 'Cuatrenio to refresh')
-program.requiredOption('--legislatura <string>', 'Legislatura to refresh')
-program.requiredOption('--tipo <string>', 'detalle | lista')
+program.option('--legislatura <string>', 'Legislatura to refresh')
+program.option('--tipo <string>', 'detalle | lista | full')
 program.action(main)
 
 async function main(options: any) {
-  const { cuatrenio, legislatura, tipo } = z
-    .object({ cuatrenio: z.string(), legislatura: z.string(), tipo: z.enum(['detalle', 'lista']) })
-    .parse(options)
-  const senadoService = new SenadoService()
-  if (tipo === 'lista') {
-    await senadoService.refreshProyectos(cuatrenio, legislatura)
-  } else {
-    await senadoService.refreshProyectoDetalles(legislatura)
-  }
+    const { cuatrenio, legislatura, tipo } = z
+        .object({ cuatrenio: z.string(), legislatura: z.string().optional(), tipo: z.enum(['detalle', 'lista', 'full']).default('full') })
+        .parse(options)
+    const senadoService = new SenadoService()
+    if (tipo === 'full') {
+        if (legislatura) {
+            return await senadoService.refreshLegislatura(cuatrenio, legislatura)
+        } else {
+            return await senadoService.refreshCuatrenio(cuatrenio)
+        }
+    } else if (tipo === 'lista') {
+        if (!legislatura) {
+            throw new Error('Legislatura is required')
+        }
+        await senadoService.refreshProyectos(cuatrenio, legislatura)
+    } else if (tipo === 'detalle') {
+        if (!legislatura) {
+            throw new Error('Legislatura is required')
+        }
+        await senadoService.refreshProyectoDetalles(legislatura)
+    } else {
+        throw new Error(`Invalid tipo: ${tipo}`)
+    }
 }
 
 program.parse(process.argv)
